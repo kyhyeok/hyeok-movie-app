e36bb26e1655395eb00ece9836c57cdfcf743595
kyhyeok <kyhyeok@gmail.com>
2020-04-03 09:04:48 +0900

    movie-app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, rating, runtime, genres, summary, poster }) {
+function Movie({ id, year, title, rating, runtime, genres, summary, poster }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <ul className="movie__info">
-          <li className="info__etc">{year} | 개봉</li>
-          <li className="info__etc"> | {runtime}분 |</li>
-          <li className="info__etc">평점 | {rating}</li>
-        </ul>
-        <ul className="movie__genres">
-          <li className="genres__genre">장르 |</li>
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 170)}...</p>
-      </div>
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            rating,
+            runtime,
+            genres,
+            summary,
+            poster
+          }
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <ul className="movie__info">
+            <li className="info__etc">{year} | 개봉</li>
+            <li className="info__etc"> | {runtime}분 |</li>
+            <li className="info__etc">평점 | {rating}</li>
+          </ul>
+          <ul className="movie__genres">
+            <li className="genres__genre">장르 |</li>
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 170)}...</p>
+        </div>
+      </Link>
     </div>
   );
 }
